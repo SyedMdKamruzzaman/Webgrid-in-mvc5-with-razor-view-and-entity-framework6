2dc9cb3df1e113e6d7de92ff1b2bf456ec873476
techstrikers <ajaysaptaputre@techstrikers.com>
2016-01-17 23:25:51 +0530

    CheckIn

---
+++ MVCDisplayWebGrid/Controllers/EmployeeController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVCDisplayWebGrid.Models;
+
+namespace MVCDisplayWebGrid.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        public ActionResult Index()
+        {
+            EmployeeDBContext db = new EmployeeDBContext();
+            return View(db.Employees.ToList());
+        }
+	}
+}
